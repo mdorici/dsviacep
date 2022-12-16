baee1379d5eae1f4533f6831ac59d7dcbd9acb61
mdorici <marianadorici@gmail.com>
2022-12-15 21:42:09 -0300

    Checking address object

--- js/controllers/form-controller.js
+++ js/controllers/form-controller.js
@@ -65,14 +65,32 @@ async function handleInputCepChange(event) {
     }
 }
 
-async function handleBtnSaveClick(event) {
+function handleBtnSaveClick(event) {
     event.preventDefault();
-    listController.addCard(state.address);
+
+    const errors = addressService.getErrors(state.address);
+
+    const keys = Object.keys(errors);
+
+    if(keys.length > 0) {
+
+        keys.forEach(key => {
+            setFormError(key, errors[key]);
+        })
+
+        //for(let i = 0; i < keys.length; i++) {
+        //    setFormError(keys[i], errors[keys[i]]);
+       // }
+    }
+    else {
+        listController.addCard(state.address);
+        clearForm();
+    }
 }
 
 function handleInputNumberChange(event) {
     if(event.target.value == "") {
-        setFormError("number", "Campo requerido!");
+        setFormError("number", "Campo requerido");
     }
     else {
         setFormError("number", "");
@@ -92,6 +110,8 @@ function clearForm() {
 
     setFormError("cep", "");
     setFormError("number", "");
+
+    state.address = new Address();
 
     state.inputCep.focus();
 }
